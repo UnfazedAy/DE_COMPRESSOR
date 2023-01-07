6b86259aac8f638acabf8f00100c31283f145750
victor <nwosuvictor54@gmail.com>
2023-01-07 18:07:10 +0400

    added venv to gitignore

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jedda/Desktop/Decompressor/backend/venv"
+set -gx VIRTUAL_ENV "/home/victor001/DE_COMPRESSOR/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
