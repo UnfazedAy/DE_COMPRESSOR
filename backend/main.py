d08a5c01619a18baffd17809364aefe7129b9745
Ayomide Soniyi <ayomidesoniyi@gmail.com>
2023-01-09 10:20:46 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from decompressor import app
-
+from os import getenv
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    port = getenv('PORT')
+    if not port:
+        port = '5000'
+    app.run(debug=True, host='0.0.0.0', port=port)
