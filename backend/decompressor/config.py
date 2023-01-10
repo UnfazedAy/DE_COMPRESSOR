a36419868a4c87a002908599c584d8f8f417762a
Ayomide Soniyi <ayomidesoniyi@gmail.com>
2023-01-10 23:54:48 +0100

    Testing a new update

--- backend/decompressor/config.py
+++ backend/decompressor/config.py
@@ -14,7 +14,7 @@ class Config:
 
 class DevConfig(Config):
     # SQLALCHEMY_DATABASE_URI='postgresql:///'+os.path.join(Basedir, 'dev.db')
-    SQLALCHEMY_DATABASE_URI=f'postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}:5432/{DATABASE}'
+    SQLALCHEMY_DATABASE_URI=f'postgres://{USERNAME}:{PASSWORD}@{HOSTNAME}:5432/{DATABASE}'
     DEBUG=True
     SQLALCHEMY_ECHO=True
 
