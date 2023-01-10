b5a62adb18f04d8cd5d6ab091465caa133980617
victor <nwosuvictor54@gmail.com>
2023-01-10 05:29:57 +0400

    .

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from decompressor import app
-from os import getenv
-
-
-if __name__ == "__main__":
-    port = getenv('PORT')
-    if not port:
-        port = '5000'
-    app.run(debug=True, host='0.0.0.0', port=port)
