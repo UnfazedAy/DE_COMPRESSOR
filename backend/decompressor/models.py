806efaa35f7318f8c1a39db5ace9b3da6520ae62
jeddaa <fathiato@gmail.com>
2023-01-13 07:56:09 +0100

    updating the database model

--- backend/decompressor/models.py
+++ backend/decompressor/models.py
@@ -15,7 +15,7 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(120), nullable = False, unique=True)
     password = db.Column(db.String(), nullable = False)
-    images = db.relationship('Images', backref='user')
+    images = db.relationship('Images', backref='user', cascade='all, delete-orphan')
 
 
 
